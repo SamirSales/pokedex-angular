aad3d93088cc742ab8eccc420bfdbf1d41978297
Samir Sales <samir.sribeiro@gmail.com>
2021-11-21 20:09:39 -0300

    #master create app-table (in progress)

--- src/app/components/pokemon-table/pokemon-table.component.ts
+++ src/app/components/pokemon-table/pokemon-table.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pokemon-table',
-  templateUrl: './pokemon-table.component.html',
-  styleUrls: ['./pokemon-table.component.css']
+    selector: 'app-pokemon-table',
+    templateUrl: './pokemon-table.component.html',
+    styleUrls: ['./pokemon-table.component.css']
 })
 export class PokemonTableComponent implements OnInit {
+    headers = [
+        { text: 'Number', input: 'number' },
+        { text: 'Image', input: 'image' },
+        { text: 'Name', input: 'name' },
+        { text: 'Type', input: 'type' }
+    ];
 
-  constructor() { }
+    pokemons = [
+        { number: 1, image: 'src1', name: 'name1', type: 'type1' },
+        { number: 2, image: 'src2', name: 'name2', type: 'type2' },
+        { number: 3, image: 'src3', name: 'name3', type: 'type3' },
+        { number: 4, image: 'src4', name: 'name4', type: 'type4' },
+        { number: 5, image: 'src5', name: 'name5', type: 'type5' },
+        { number: 6, image: 'src6', name: 'name6', type: 'type6' },
+        { number: 7, image: 'src7', name: 'name7', type: 'type7' },
+        { number: 8, image: 'src8', name: 'name8', type: 'type8' }
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {}
 }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+    selector: 'app-table',
+    templateUrl: './table.component.html',
+    styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+    @Input() headers: any = [
+        //text, input
+    ];
 
-  constructor() { }
+    @Input() items: any = [];
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {}
+
+    getInputs(): any {
+        return this.headers.map((header: any) => header.input);
+    }
 }
