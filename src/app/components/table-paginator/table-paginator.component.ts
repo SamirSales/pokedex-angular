412a7b45e4c0eca4bf11d2fda3a0f0fafd584f8f
Samir Sales <samir.sribeiro@gmail.com>
2021-11-27 20:32:13 -0300

    #master table-paginator (update)

--- src/app/components/table-paginator/table-paginator.component.ts
+++ src/app/components/table-paginator/table-paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'app-table-paginator',
@@ -6,41 +6,84 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./table-paginator.component.css']
 })
 export class TablePaginatorComponent implements OnInit {
+    @ViewChild('tablePaginatorContainer', { static: false }) tablePaginatorContainer: ElementRef;
+
     pageQuantity = 30;
     selectedIndex = 4;
+    widthContainer = 0;
 
-    constructor() {}
+    constructor(tablePaginatorContainer: ElementRef, private changeDetectorRef: ChangeDetectorRef) {
+        this.tablePaginatorContainer = tablePaginatorContainer;
+    }
 
     ngOnInit(): void {}
 
+    ngAfterViewInit() {
+        this.widthContainer = this.tablePaginatorContainer.nativeElement.offsetWidth;
+        this.changeDetectorRef.detectChanges();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('changes', changes);
+        // changes of inputs
+    }
+
     getElements() {
         const elements = [];
+        const middleIndex = this.getMiddleIndex();
+        const isPageQuantityToLarge = this.isPageQuantityToLarge();
 
         for (let i = 1; i <= this.pageQuantity; i++) {
-            if (this.isPageQuantityToLarge() && i === this.getMiddleIndex()) {
-                elements.push({
-                    value: '...',
-                    class: 'three-points',
-                    hasAction: false
-                });
+            if (isPageQuantityToLarge && i === middleIndex) {
+                elements.push(this.getThreePointsButtonElement());
+                i = this.pageQuantity - middleIndex;
             } else {
-                elements.push({
-                    value: i,
-                    class: i === this.selectedIndex ? 'selected' : '',
-                    hasAction: true
-                });
+                elements.push(this.getButtonElementByValue(i));
             }
         }
 
         return elements;
     }
 
+    getThreePointsButtonElement() {
+        return {
+            value: '...',
+            class: 'three-points',
+            hasAction: false
+        };
+    }
+
+    getButtonElementByValue(value: number) {
+        return {
+            value: value,
+            class: value === this.selectedIndex ? 'selected' : '',
+            hasAction: true
+        };
+    }
+
     getMiddleIndex() {
-        return Math.floor(this.pageQuantity / 2);
+        return Math.floor(this.getPageButtonQuantity() / 2);
+    }
+
+    getPageButtonQuantity() {
+        const arrowButtonQuantity = 2;
+        const threePointsButtonQuantity = 1;
+        const buttonQuantityPossible = this.getButtonQuantityPossible();
+
+        if (this.isPageQuantityToLarge()) {
+            return buttonQuantityPossible - arrowButtonQuantity - threePointsButtonQuantity;
+        }
+        return this.pageQuantity;
     }
 
     isPageQuantityToLarge() {
-        return true;
+        const buttonQuantityPossible = this.getButtonQuantityPossible();
+        return this.pageQuantity > buttonQuantityPossible;
+    }
+
+    getButtonQuantityPossible() {
+        const buttonWidth = 44;
+        return Math.floor(this.widthContainer / buttonWidth) - 2;
     }
 
     onClick(element: any) {
