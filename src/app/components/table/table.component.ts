0c2f6c7c0cbb6f238852c28d246684b3e4bca4c5
Samir Sales <samir.sribeiro@gmail.com>
2022-01-10 21:25:15 -0300

    #master show pokemon images on the table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, TemplateRef } from '@angular/core';
 
 @Component({
     selector: 'app-table',
@@ -11,6 +11,8 @@ export class TableComponent implements OnInit {
     ];
 
     @Input() items: any = [];
+
+    @ContentChild(TemplateRef) templateRef: any;
 
     constructor() {}
 
