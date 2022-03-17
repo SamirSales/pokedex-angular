e733fea4a5852c5e27132d053110ac8f71639f98
Samir Sales <samir.sribeiro@gmail.com>
2022-03-17 16:57:34 -0300

    #master src/app/views/more-info-view/more-info-view.component (development)

--- src/app/views/more-info-view/more-info-view.component.ts
+++ src/app/views/more-info-view/more-info-view.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoutePath } from '../../app-routing.module';
 
 @Component({
-  selector: 'app-more-info-view',
-  templateUrl: './more-info-view.component.html',
-  styleUrls: ['./more-info-view.component.css']
+    selector: 'app-more-info-view',
+    templateUrl: './more-info-view.component.html',
+    styleUrls: ['./more-info-view.component.css']
 })
 export class MoreInfoViewComponent implements OnInit {
+    constructor(private router: Router) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    goToHomePage() {
+        this.router.navigateByUrl('/' + RoutePath.POKEMONS);
+    }
 }
