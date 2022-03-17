ccbf77ed80caf36010a94c1e61477d30f040d7d6
Samir Sales <samir.sribeiro@gmail.com>
2022-03-17 16:08:10 -0300

    #master fix animation on dropdown.component

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,14 +1,40 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-dropdown',
     templateUrl: './dropdown.component.html',
-    styleUrls: ['./dropdown.component.css']
+    styleUrls: ['./dropdown.component.css'],
+    animations: [
+        trigger('dropdownState', [
+            state(
+                'visible',
+                style({
+                    transform: 'translateY(0)',
+                    display: 'block',
+                    opacity: 1
+                })
+            ),
+
+            state(
+                'hide',
+                style({
+                    transform: 'translateY(-10px)',
+                    display: 'none',
+                    opacity: 0
+                })
+            ),
+
+            transition('visible <=> hide', animate(100))
+        ])
+    ]
 })
 export class DropdownComponent implements OnInit {
     @Input() value: string = '';
     @Input() options: string[] = [];
     @Output() select = new EventEmitter<string>();
+
+    dropdownState: string = 'hide';
 
     constructor() {}
 
@@ -23,5 +49,14 @@ export class DropdownComponent implements OnInit {
 
     onSelectOption(option: string) {
         this.select.emit(option);
+        this.dropdownState = 'hide';
+    }
+
+    onMouveEnter() {
+        this.dropdownState = 'visible';
+    }
+
+    onMouveLeave() {
+        this.dropdownState = 'hide';
     }
 }
